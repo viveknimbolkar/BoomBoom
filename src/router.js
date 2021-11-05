bf9e47c902ea18a1aaf7aac304f8c6a793235284
viveknimbolkar <viveknimbolkar50@gmail.com>
2021-11-05 22:48:39 +0530

    add explore, comming-soon api engpoints and respective views

--- src/router.js
+++ src/router.js
@@ -10,4 +10,12 @@ router.get('/theme1', (req, res) => {
     res.render('theme-1/theme1'); 
 });
 
+router.get('/comming', (req, res) => {
+    res.render('comming-soon/comming-soon'); 
+});
+
+router.get('/explore', (req, res) => {
+    res.render('explore/explore'); 
+});
+
 module.exports = router;
