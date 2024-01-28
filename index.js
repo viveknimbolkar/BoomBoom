3e6d8f9479315fe3e04ef9fd07df51303f503d0c
viveknimbolkar <viveknimbolkar50@gmail.com>
2024-01-28 15:10:46 +0530

    update serverless deps

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const handler = require("serverless-express/handler");
-const app = require("./app.js");
-module.exports.api = handler(app);
+'use strict'
+const awsServerlessExpress = require('aws-serverless-express')
+const app = require('./app')
+const server = awsServerlessExpress.createServer(app)
+
+exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
